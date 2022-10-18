8f9121e2b2a05abaf80f3b0722377c731c4aca4d
ManoilovaKaterina <99344841+ManoilovaKaterina@users.noreply.github.com>
2022-10-18 18:16:24 +0300

    Update YOPIlab1.py

--- probability/YOPIlab1.py
+++ probability/YOPIlab1.py
@@ -2,14 +2,6 @@ import matplotlib.pyplot as plt
 import time
 import math
 
-file10 = open('C:/Users/undor/OneDrive/Рабочий стол/task_01_data/input_10.txt', 'r')
-file100 = open('C:/Users/undor/OneDrive/Рабочий стол/task_01_data/input_100.txt', 'r')
-file1000 = open('C:/Users/undor/OneDrive/Рабочий стол/task_01_data/input_1000.txt', 'r')
-
-fileout10 = open("C:/Users/undor/OneDrive/Рабочий стол/task_01_data/output10.txt", "w")
-fileout100 = open("C:/Users/undor/OneDrive/Рабочий стол/task_01_data/output100.txt", "w")
-fileout1000 = open("C:/Users/undor/OneDrive/Рабочий стол/task_01_data/output1000.txt", "w")
-
 def out(arr):
     print("Список елементів: ", end=" ")
     for el in arr:
@@ -40,7 +32,7 @@ def countCuFr(freqarr):
         fr += freqarr[el][0]
         freqarr[el][1] = fr
 
-def findMod(freqarr):
+def findMod(freqarr, fileout):
     vallist = []
     for el in freqarr.keys():
         vallist.append(freqarr[el][0])
@@ -52,18 +44,11 @@ def findMod(freqarr):
         for el in freqarr.keys():
             if freqarr[el][0]==mv:
                 print("Мода: ", el)
+                fileout.write("\nМода: " + str(el))
 
 def findMid(arr):
     mid = sum(arr)/len(arr)
     return mid
-
-def MAD(arr, freqarr):
-    sum1 = 0
-    mid = findMid(arr)
-    for el in freqarr.keys():
-        sum1 += freqarr[el][0]*abs(int(el)-mid)
-    mad = sum1/len(arr)
-    return mad
 
 def dispersion(arr, freqarr):
     sum1 = 0
@@ -106,10 +91,15 @@ def writeOutput(Lines, fileout):
     for el in freqArr.keys():
         tempstring = str(el) + "\t| " + str(freqArr[el][0]) + "\t  |\t" + str(freqArr[el][1]) + "\n"
         fileout.write(tempstring)
+        fileout.flush()
     fileout.write("\nКількість елементів: " + str(len(Lines)))
-    fileout.write("\nСереднє абсолютне відхилення = "+ str(MAD(Lines, freqArr)))
+    fileout.flush()
+    fileout.write("\nМедіана: " + str(median(Lines)))
+    fileout.flush()
     fileout.write("\nДисперсія = " + str(dispersion(Lines, freqArr)))
+    fileout.flush()
     fileout.write("\nСереднє квадратичне (стандартне) відхилення = " + str(math.sqrt(dispersion(Lines, freqArr))))
+    fileout.flush()
 
 def scenario(file, fileout):
     start_time = time.time()
@@ -117,9 +107,8 @@ def scenario(file, fileout):
     LineString = file.read().splitlines()
     Lines = [int(x) for x in LineString]
     Lines.remove(Lines[0])
+    Lines = sorted(Lines)
 
-    if file!=file1000:
-        out(Lines)
     print("\nКількість елементів: ", len(Lines))
     findMax(Lines)
     print("Медіана: ", median(Lines))
@@ -129,17 +118,13 @@ def scenario(file, fileout):
     print("Елемент | Частота | Сукупна частота")
     for el in freqArr.keys():
         print(el, "\t| ", freqArr[el][0], "\t  |\t", freqArr[el][1])
-    findMod(freqArr)
-    print("Середнє абсолютне відхилення = ", MAD(Lines, freqArr))
     print("Дисперсія = ", dispersion(Lines, freqArr))
     print("Середнє квадратичне (стандартне) відхилення = ", math.sqrt(dispersion(Lines, freqArr)))
-
     writeOutput(Lines, fileout)
+    findMod(freqArr, fileout)
+    fileout.flush()
+
     fileout.close
-    if file==file10:
-        bardiagram()
-        print("--- час виконання: %s секунд ---" % (time.time() - start_time))
-        plt.show()
     histogram(Lines)
     print("--- час виконання: %s секунд ---" % (time.time() - start_time))
     plt.show()
@@ -147,12 +132,12 @@ def scenario(file, fileout):
 def menu():
     print("Введіть кількість елементів файлу (10, 100, 1000): ", end=" ")
     comm = input()
-    if comm == "10":
-        scenario(file10, fileout10)
-    elif comm == "100":
-        scenario(file100, fileout100)
-    elif comm == "1000":
-        scenario(file1000, fileout1000)
+    if comm == "10" or comm == "100" or comm == "1000":
+        filestring = "C:/Users/undor/OneDrive/Рабочий стол/task_01_data/input_" + comm +".txt"
+        fileoutstring = "C:/Users/undor/OneDrive/Рабочий стол/task_01_data/output" + comm +".txt"
+        file = open(filestring, 'r')
+        fileout = open(fileoutstring, 'w')
+        scenario(file, fileout)
     else:
         print("Введеної кількості елементів не існує, спробуйте ще: ")
         menu()
