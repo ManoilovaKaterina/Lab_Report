3529621f006172f0c2df26d292654b28dfa05867
ManoilovaKaterina <99344841+ManoilovaKaterina@users.noreply.github.com>
2023-04-10 19:07:54 +0300

    Add files via upload

---
+++ ООKП/s2lab3/Wares.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace ookplab3
+{
+    public interface IItem
+    {
+        int Id { get; set; }
+    }
+    public class Item : IItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime SupplyDate { get; set; }
+        public double Weight { get; set; }
+        public double Width { get; set; }
+        public double Length { get; set; }
+        public double Height { get; set; }
+    }
+
+    public class ItemsContext : DbContext
+    {
+        public ItemsContext() : base("DefaultConnection") { }
+        public DbSet<Item> Items { get; set; }
+    }
+}
